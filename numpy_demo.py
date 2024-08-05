9665a1c033125dfacd3df879014c6d9a084b7a1e
venom <venom205080@gmail.com>
2024-08-05 11:48:11 +0530

    modified nympy_demo files

---
+++ numpy_demo.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a = np.array([1,2,3,4])
+two_d = np.array([[1,2,3], [4,5,6], [7,8,9]])
+a[0] = 12
+b = a[1:]
+b[0] = 23
+# print(b[:])
+# print(a[:])
+print(two_d)
+print(two_d[1,2])
+print(two_d[1][2])
