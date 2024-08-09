889a1fb82c2c12fb6e7615bafee440ee30f95cc8
venom <venom205080@gmail.com>
2024-08-09 18:55:06 +0530

    commiting everything fast api post demo and connecting pandas with snowflake

--- 7aug2024/fast_api_demo/my_project.py
+++ 7aug2024/fast_api_demo/my_project.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from pydantic import BaseModel
+import random as r
+# from typing import Annotated
 
 import requests as req
 
@@ -11,12 +13,15 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+# function to generate random color
+def rand_color():
+    return f"rgb({r.randint(0, 255)}, {r.randint(0, 255)}, {r.randint(0, 255)})"
+
 class Register(BaseModel):
     email: str
     password: str
 
-dummy_DB = [{"email": "ram123@gmail.com", "password": "itsasecretpassword"},
-            {"email": "mynameis@gmail.com", "password":"angelpriya"}]
+dummy_DB = [{"title": "talk to ashok", "desc":"have to contact ashok to send azure access"}]
 
 @app.get("/", response_class=HTMLResponse)
 async def home():
@@ -31,7 +36,7 @@ async def get_user(request: Request,id:str):
     user_details = res[int(id)-1]
     return templates.TemplateResponse(request=request,name="show_user.html", context={"user": user_details})
 
-
+# using json
 @app.post("/register")
 async def register_user(user:Register):
     email = user.email
@@ -41,9 +46,22 @@ async def register_user(user:Register):
         "email": email,
         "password": password,
     }
+
+
+# using form
+@app.get("/notes", response_class=HTMLResponse)
+async def get_notes(request: Request):
+    return templates.TemplateResponse(request=request,name="notes.html", context={"notes": dummy_DB, "color": rand_color})
+
     
+@app.post("/notes")
+async def set_notes(title: str=Form(), desc: str= Form()):
+    dummy_DB.append({"title": title, "desc": desc})
+    return RedirectResponse(url="/notes", status_code=303)
+
 
     
 
+    
 # query param example
 # @app.get("users")
--- snowflake_demo.py
+++ snowflake_demo.py
@@ -1,4 +1,6 @@
 import snowflake.connector
+import pandas as pd
+from snowflake.connector.pandas_tools import write_pandas
 
 
 # Create a connection object
@@ -7,8 +9,8 @@ conn = snowflake.connector.connect(
     password='123456789@aA',
     account='ofdxezx-lr56952',
     warehouse='COMPUTE_WH',
-    database='department',
-    schema='public'
+    database='DEPARTMENT',
+    schema='MY_SCHEMA'
 )
 
 
@@ -28,7 +30,36 @@ results = cur.fetchall()
 # Print the results
 for row in results:
     print(row)
+    
+    
+#--------- extracting data from snowflake to pandas dataframe----------
+# 1 . fetch pandas all
+# query = "SELECT * FROM EMPLOYEE"
+# cur.execute(query)
+# emp_df = cur.fetch_pandas_all()
+# print(emp_df.head(2))
 
+# 2. fetch pandas batches-->fetches the data row by row 
+# for df in cur.fetch_pandas_batches():
+#     print(df)
+
+# ----------Writes a pandas DataFrame to a table in a Snowflake database.----------
+##note: you might have to use double quote in table name while querying data from dataframe to snowflake 
+##explanation -  perhaps the reason for that is that snowflake by default stores table or database or schema name in upper case , so if you name your table/db/schema upper case then you need not to use double quotes but if you give in lower case then you may have to use double quotes with your table or db or schema names.
+
+# Mobile_Price_Prediction_df = pd.read_csv('Mobile-Price-Prediction-cleaned_data.csv')
+# # print(salary.columns)
+# write_pandas(conn, Mobile_Price_Prediction_df, table_name="mob_price_pred", auto_create_table=True)
+# query = 'SELECT * FROM "mob_price_pred"'    #<------ see here we have to use double quotes with table name
+# cur.execute(query)
+# df = cur.fetch_pandas_all()
+# print(df)
+
+
+
+# df = pd.DataFrame([('Mark', 10), ('Luke', 20)], columns=['name', 'balance'])
+# print(df.columns)
+# write_pandas(conn, df, 'customers', auto_create_table=True)
 # # Close the cursor and connection
 cur.close()
 conn.close()
